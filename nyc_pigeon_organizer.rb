4030b388885c54a0c9ff305c7be6d50730c7d89d
Zach Bayard <zbayard@gmail.com>
2020-11-02 16:29:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
 def nyc_pigeon_organizer(data)
+  pigeon_list = {
+    "Theo" => {
+      :color => ["purple", "grey"],
+      :gender => ["male"],
+      :lives => ["Subway"]
+    },
+    "Peter Jr." => {
+      :color => ["purple", "grey"],
+      :gender => ["male"],
+      :lives => ["Library"]
+    },
+    "Lucky" => {
+      :color => ["purple"],
+      :gender => ["male"],
+      :lives => ["Central Park"]
+    },
+    "Ms. K" => {
+      :color => ["grey", "white"],
+      :gender => ["female"],
+      :lives => ["Central Park"]
+    },
+    "Queenie" => {
+      :color => ["white", "brown"],
+      :gender => ["female"],
+      :lives => ["Subway"]
+    },
+    "Andrew" => {
+      :color => ["white"],
+      :gender => ["male"],
+      :lives => ["City Hall"]
+    },
+    "Alex" => {
+      :color => ["white", "brown"],
+      :gender => ["male"],
+      :lives => ["Central Park"]
+    }
+  }
   # write your code here!
 end
