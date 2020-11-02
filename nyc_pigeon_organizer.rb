bb9669eaee851650c7567a9df073d052218579a5
Zach Bayard <zbayard@gmail.com>
2020-11-02 16:38:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
+  final_results = data.each_with_object({}) do | (key, value) , final_array |
+    value.each do |inner_key, names |
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key.to_s)
+      end
+    end
+  end
   # write your code here!
 end
