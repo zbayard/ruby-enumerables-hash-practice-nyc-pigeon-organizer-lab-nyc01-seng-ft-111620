21bf94041f931e20aa1f3e7dfd732863b3f85a1c
Zach Bayard <zbayard@gmail.com>
2020-11-02 16:38:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,40 +1,16 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = {
-    "Theo" => {
-      :color => ["purple", "grey"],
-      :gender => ["male"],
-      :lives => ["Subway"]
-    },
-    "Peter Jr." => {
-      :color => ["purple", "grey"],
-      :gender => ["male"],
-      :lives => ["Library"]
-    },
-    "Lucky" => {
-      :color => ["purple"],
-      :gender => ["male"],
-      :lives => ["Central Park"]
-    },
-    "Ms. K" => {
-      :color => ["grey", "white"],
-      :gender => ["female"],
-      :lives => ["Central Park"]
-    },
-    "Queenie" => {
-      :color => ["white", "brown"],
-      :gender => ["female"],
-      :lives => ["Subway"]
-    },
-    "Andrew" => {
-      :color => ["white"],
-      :gender => ["male"],
-      :lives => ["City Hall"]
-    },
-    "Alex" => {
-      :color => ["white", "brown"],
-      :gender => ["male"],
-      :lives => ["Central Park"]
-    }
-  }
+  final_results = data.each_with_object({}) do | (key, value) , final_array |
+    value.each do |inner_key, names |
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key.to_s)
+      end
+    end
+  end
   # write your code here!
 end
